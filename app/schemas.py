b141b8b36b243ab6b86599232809f363c6dbf36c
Luke Nisbet <you@example.com>
2026-02-25 16:04:19 +0900

    update schema to make merchant_category optional

--- app/schemas.py
+++ app/schemas.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 class TransactionIn(BaseModel):
     account_id: str
     transaction_amount: float = Field(gt=0, description="Amount must be greater than 0")
-    merchant_category: str
+    merchant_category: str = "unknown" # Now, if the user leaves it blank, Pydantic fills in "unknown"
     customer_age: int = Field(ge=18, le=120, description="Customer must be an adult")
 
 class FraudPredictionOut(BaseModel):
